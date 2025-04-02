2a26449659576f3828fa89f34770bb01ac25db28
snigudkar <146969559+snigudkar@users.noreply.github.com>
2025-04-02 11:33:04 +0530

    update #3

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCr8vnJlM6XxNreyl2ZRJHSAco2s8v_j8U",
-    authDomain: "codebid-auth.firebaseapp.com",
-    projectId: "codebid-auth",
-    storageBucket: "codebid-auth.firebasestorage.app",
-    messagingSenderId: "272160224",
-    appId: "1:272160224:web:3c215710e118ff2fa70785"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCr8vnJlM6XxNreyl2ZRJHSAco2s8v_j8U",
+//     authDomain: "codebid-auth.firebaseapp.com",
+//     projectId: "codebid-auth",
+//     storageBucket: "codebid-auth.firebasestorage.app",
+//     messagingSenderId: "272160224",
+//     appId: "1:272160224:web:3c215710e118ff2fa70785"
+//   };
 
 
 
